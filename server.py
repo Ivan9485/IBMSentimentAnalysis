b2672f2abafe41db325f4d6676bdac1664aebc8f
“Ivan” <pascual_815@hotmail.com>
2024-10-01 21:31:19 -0400

    Unit tests included

--- server.py
+++ server.py
@@ -3,10 +3,13 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
+
+from flask import Flask, render_template,request
+
 # Import the sentiment_analyzer function from the package created: TODO
-
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 #Initiate the flask app : TODO
-
+app = Flask("Wattson Sent Analyzer")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
